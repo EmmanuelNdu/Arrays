bc31faded49ecebd200c4422c47c611d3e5f1875
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-10-29 11:22:32 +0100

    Mapping Array Methods

---
+++ map_ArrayMethods.js
@@ -0,0 +1,27 @@
+// const products = [
+//     {
+//         name:'Laptop',
+//         price:1500,
+//         count:5
+//     },
+//     {
+//         name: 'desktop',
+//         price: 1000,
+//         count:2
+//     },
+//     {
+//         name: 'phone',
+//         price: 500,
+//         count: 10
+//     }
+// ];
+
+// const totalProductsValue = products.map(item => ({name:item.name,totalValue: item.price * item.count}));
+// console.log(totalProductsValue)
+
+
+
+const str = ['1', '2', '3', '4']; // Converting Array of string into numbers
+
+const numbers = str.map(Number);
+console.log(numbers);
