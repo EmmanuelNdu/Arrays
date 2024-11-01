7ea01d9ccb0f04b58233bf3b2feac5e734f01f7d
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-01 22:55:14 +0100

    Concat Array

---
+++ concatArray.js
@@ -0,0 +1,14 @@
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+const d = [7, 8, 9];
+
+//const c = a.concat();
+
+//console.log(c);
+
+// c.push(4);
+// console.log(c);
+console.log(a);
+
+const c = a.concat(1, 2, a);
+console.log(c);
