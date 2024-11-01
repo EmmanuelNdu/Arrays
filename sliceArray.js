55b65c5a40ce468e209ceaefbb50d5b043b3169e
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-01 19:59:23 +0100

    Array.slice()

---
+++ sliceArray.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+const numbers2 = numbers.slice(-2);// To get the last 2 items in the array
+// slice(1) to get all the item starting from 1 in the array
+
+console.log(numbers);
+console.log(numbers2);
+
+const participants = ['Florin', 'Ivan','Liam', 'Jam', 'patrick'];
+
+const winners = participants.slice(0,3);
+console.log(winners)
