ab36d258d44272f80f93b5adec8ce78ca504bd55
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-02 18:57:37 +0100

    Shift() array

---
+++ shiftArray.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3];
+
+// numbers.shift();  // This will remove the number at the beginning of the array
+
+const firstItem = numbers.shift()
+
+console.log(firstItem);
+
+// console.log(numbers);
