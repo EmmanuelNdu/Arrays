05c59d047597b495f39777c0f4ce088ae968cfc2
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-02 03:12:02 +0100

    Include Array ()

---
+++ includeArray.js
@@ -0,0 +1,14 @@
+const names = ['Florin', 'Ivan','Liam', 'Jam'];
+
+const res = names.includes('Ivan');
+
+console.log(res);
+
+const fruits = ['orange', 'mango', 'Apple', 'watermelon'];
+const pes = fruits.includes('mango');
+
+if (pes) {
+    console.log('Yuppy!')
+} else {
+    console.log('sadly... let us  buy an orange');
+}
