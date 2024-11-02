ad590c758e8a8c90c341673252937119b7690806
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-02 18:23:13 +0100

    Reverse Array

---
+++ reverseArray.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5];
+const newArr = numbers.concat().reverse();
+console.log(numbers);
+console.log(newArr);
+
+const str = "Coding is fun!"
+ const res = str 
+ .split('')
+ .reverse()
+ .join('');
+ console.log(res);
