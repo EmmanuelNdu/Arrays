b936d9df58937e9d28d4b681f4273cb5045b877e
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-02 20:28:18 +0100

    indexOf() Array

---
+++ indexOfArray.js
@@ -0,0 +1,11 @@
+const names = ['Florin', 'Ivan', 'Liam', 'Paul', 'Ivan'];
+
+const idx = names.lastIndexOf('Liam'); // Last index will return the last index from the array
+
+console.log(idx);
+
+// if (idx > -1) {
+//     console.log('Yay!, we have Paul');
+// } else {
+//     console.log('sad!');
+// }
