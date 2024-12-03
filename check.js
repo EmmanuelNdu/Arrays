19c30232e9c64fe4e7c338f26aa391dd5e6e2b7a
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-03 21:45:42 +0100

    key

--- check.js
+++ check.js
@@ -7,10 +7,20 @@ const person = {  // person is an object which holds the key value pair
 };
 //console.log(person.age)
 //For...in
- for(const liam in person) {
-    const value = person[liam]
+//  for(const liam in person) {
+//     const value = person[liam]
 
-    console.log(liam, value)
- }
+//     console.log(liam, value)
+//  }
 
- console.log("name" in person)
+//  console.log("name" in person)
+
+// object.keys()
+
+const keys = Object.keys(person)
+
+keys.forEach(key => {
+    const value = person[key]
+
+    console.log(key, value)
+})
