84ba4e34a21317a78138c5c4fc5a9adb5f062aeb
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-04 02:51:11 +0100

    Array

--- rest.js
+++ rest.js
@@ -1,3 +1,5 @@
+// rest Operator in an object
+
 const take = {name:"Bola", 
     school:"Yabatech",
     Street:"Yaba",
@@ -9,3 +11,13 @@ const take = {name:"Bola",
 const {name, ...must} = take;
 
 console.log(name ,must)
+
+// rest operator in an array
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+ // destructuring the arrays
+
+ const [first, second, ...rest ] = numbers;
+
+ console.log(first, second, ...rest);
+ console.log(...rest)
